7a4c559346d649f8f6dbd8890ce8981d545e7d48
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-02 21:30:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+Handlebars.registerHelper('comment_body', function() {
+  if(this.state === "closed") {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+  }
+})
+
 function loadIssue() {
   var issue = {
     state: "closed",
@@ -10,5 +18,5 @@ function loadIssue() {
   // for (var i = 0; i < issues.length; i++) {
     var result = template(issues);
     document.getElementsByTagName("main")[0].innerHTML += result;
-  
+  // }
 }
