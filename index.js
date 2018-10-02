7e51a27495e20f9238a76af1ae8cac76846be922
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-02 21:21:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function loadIssue() {
   }
 
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  for (var i = 0; i < issues.length; i++) {
-    var result = template(issues[i]);
+  // for (var i = 0; i < issues.length; i++) {
+    var result = template(issues);
     document.getElementsByTagName("main")[0].innerHTML += result;
   }
 }
