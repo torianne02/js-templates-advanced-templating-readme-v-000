59d7c4bfad1b15886da912527851a902216a8909
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-02 21:36:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-Handlebars.registerHelper('comment_body', function() {
-  if(this.state === "closed") {
-    return new Handlebars.SafeString(this.body)
-  } else {
-    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
-  }
-})
-
 function loadIssue() {
   var issue = {
     state: "closed",
@@ -13,10 +5,7 @@ function loadIssue() {
     created_at: "2016-03-31 16:23:13 UTC",
     body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
   }
-
-  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  // for (var i = 0; i < issues.length; i++) {
-    var result = template(issues);
-    document.getElementsByTagName("main")[0].innerHTML += result;
-  // }
+   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issue);
+  document.getElementsByTagName("main")[0].innerHTML += result;
 }
